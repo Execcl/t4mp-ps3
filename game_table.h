083e250516ad4a3be4b129e18d0edc874f9deae5
Execcl <74021950+Execcl@users.noreply.github.com>
2022-04-16 22:39:16 -0500

    initial push

---
+++ game_table.h
@@ -0,0 +1,19 @@
+#pragma once
+
+enum class game_table
+{
+	r_endframe = 0x586858,
+	sys_io_cellpadgetdata = 0x649A94,
+
+	scrplaceview = 0xD69D04,
+	uicontext = 0x2627B50,
+
+	r_addcmddrawstretchpic = 0x587A18,
+	cg_drawrotatedpicphysical = 0x13F1D0,
+	r_addcmddrawtext = 0x587588,
+	r_addcmddrawtextwitheffects = 0x587440,
+	db_findxassetheader = 0x2468D0,
+	r_textwidth = 0x569450,
+
+	va = 0x3C7CB0,
+};
