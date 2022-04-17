231012cee628db2d90b46c993b3c8ce97e3a8ccf
Execcl <74021950+Execcl@users.noreply.github.com>
2022-04-16 23:00:25 -0500

    fixed address

--- game_table.h
+++ game_table.h
@@ -5,13 +5,12 @@ enum class game_table
 	r_endframe = 0x586858,
 	sys_io_cellpadgetdata = 0x649A94,
 
-	scrplaceview = 0xD69D04,
+	scrplaceview = 0xB4D49C,
 	uicontext = 0x2627B50,
 
 	r_addcmddrawstretchpic = 0x587A18,
 	cg_drawrotatedpicphysical = 0x13F1D0,
 	r_addcmddrawtext = 0x587588,
-	r_addcmddrawtextwitheffects = 0x587440,
 	db_findxassetheader = 0x2468D0,
 	r_textwidth = 0x569450,
 
